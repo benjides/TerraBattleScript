dbaed0050e344fa9d333eb78eb6b525299244b48
benjides <benjergara@gmail.com>
2016-01-14 21:38:33 +0100

    Added Headers

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Terra Battle Scripts</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Skill Boosting</a></li>
+        <li><a href="6.8.php">Metal Tickets</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
