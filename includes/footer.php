69f7bfd7006bca5c9e1657b404201ae8c30ed521
benjides <benjergara@gmail.com>
2016-01-14 21:50:51 +0100

    Corrected files

--- includes/footer.php
+++ includes/footer.php
@@ -5,10 +5,10 @@
       <p>v.1.0.0</p>
     </div>
     <div class="col-xs-6 text-right">
-      <p>Created by <a href="#">@benjides</a></p>
+      <p>Created by <a href="https://github.com/benjides/">@benjides</a></p>
     </div>
   </div>
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="js/bootstarp.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
